a73daaafac94065e3b6d5cc399958b3d7447de3f
daraGilbert <dara.gilbert.2017@mumail.ie>
2020-10-27 14:07:26 +0000

    added sync fusion schedule

--- src/pages/time.js
+++ src/pages/time.js
@@ -1,101 +1,15 @@
 import React from 'react';
+import {Inject,ScheduleComponent,Day, Week,WorkWeek,Month} from '@syncfusion/ej2-react-schedule';
 
- const Time = () => (
-  <div class= "TableStyle">
-  <table id="myTable" border="1" >
-      <tr> 
-          <th id="Time">Time</th>
-          <th id="Monday">Monday</th>
-          <th id="Tuesday">Tuesday</th>
-          <th id="Wednesday">Wednesday</th>
-          <th id="Thursday">Thursday</th>
-          <th id="Friday">Friday</th>
-      </tr>
-      <tr>
-        <td contenteditable='false'>9:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>10:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>11:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>12:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>13:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>14:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>15:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>16:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>17:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-      <tr>
-        <td contenteditable='false'>18:00</td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-        <td contenteditable='true'></td>
-      </tr>
-  </table>
-  </div>
-);
+class Time extends React.Component{
+  render() {
+return <ScheduleComponent currentView='Week'>
+  <Inject services={[Day, Week, WorkWeek,Month]}/>
+</ScheduleComponent>
+  }
+}
+export default Time;
 
-export default Time; 
 
  
 
