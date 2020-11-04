18487daa7e2b227a23b4ab0352036acb97080501
Leila <leila.benhariz.2019@mumail.ie>
2020-11-04 18:33:43 +0000

    Changed the library image

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -11,7 +11,7 @@ const Resources = () => (
     <Link to="/Map"><input class="Map" class = "Mapbut" type="image" src="../Image/002-map.png" alt="Map"></input></Link>
   <a target="_blank" href="https://www.maynoothuniversity.ie/moodle"><input class="Moodlebut" type="image" src="../Image/moodle.png" alt="Moodle" ></input></a>
   <div></div>
- <a target="_blank" href="https://www.maynoothuniversity.ie/library"><input class = "Librarybut" type="image" src="../Image/036-explore.png" alt="Library"></input></a>
+ <a target="_blank" href="https://www.maynoothuniversity.ie/library"><input class = "Librarybut" type="image" src="../Image/019-cloud library.png" alt="Library"></input></a>
  <a target="_blank" href="https://www.maynoothuniversity.ie/coronavirus"><input class = "Coronabut" type="image" src="../Image/soap.png" alt="Corona Info"></input></a>
   <div></div>
     </div>
